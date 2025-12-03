41c07aba9b0c5e6dab0bf7a32b0833d3d89c18e2
leoulendryas <leoulendryas@gmail.com>
2025-12-03 12:35:41 +0300

    update: part two solution for day three

--- day03/part2.ts
+++ day03/part2.ts
@@ -1,31 +1,38 @@
+
 import { readFileSync } from "fs";
 
 const input = readFileSync("input.txt", "utf8");
 const banks = input.trim().split("\n");
-const calculate: number[] = [];
+const calculate: bigint[] = [];
 
 for (const line of banks) {
   const bank = line.split("").map(Number);
+  const topDigits: number[] = [];
+  let start = 0;
 
-  let largestNum = -1;
-  let firstIdx = -1;
-  let secondIdx = -1;
+  while (topDigits.length < 12) {
+    const remaining = 12 - topDigits.length;
 
-  for (let i = 0; i < bank.length; i++) {
-    for (let j = i + 1; j < bank.length; j++) {
-      const num = bank[i] * 10 + bank[j];
-      if (num > largestNum) {
-        largestNum = num;
-        firstIdx = i;
-        secondIdx = j;
+    const slice = bank.slice(start, bank.length - remaining + 1);
+
+    let maxDigit = -1;
+    let maxIdx = -1;
+    for (let i = 0; i < slice.length; i++) {
+      if (slice[i] > maxDigit) {
+        maxDigit = slice[i];
+        maxIdx = i;
       }
     }
+
+    topDigits.push(maxDigit);
+    start += maxIdx + 1;
   }
 
-  console.log(largestNum);
-  calculate.push(largestNum);
+  const combinedNumber = BigInt(topDigits.join(""));
+  console.log(combinedNumber.toString());
+  calculate.push(combinedNumber);
 }
 
-const answer = calculate.reduce((x, y) => x + y, 0);
-console.log(answer);
+const answer = calculate.reduce((acc, num) => acc + num, 0n);
+console.log(answer.toString());
 
